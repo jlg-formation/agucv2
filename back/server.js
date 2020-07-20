7d7b72925c2d6a428b7cffd7e381404afc5accf4
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-07-20 10:48:58 +0200

    started to implement express

--- back/server.js
+++ back/server.js
@@ -1,1 +1,18 @@
-console.log("coucou");
+const express = require("express");
+
+const app = express();
+
+app.use((req, res, next) => {
+  console.log("req.url", req.url);
+  next();
+});
+
+app.get("/", (req, res) => {
+  res.send("coucou");
+});
+
+app.get("/toto", (req, res) => {
+  res.send("titi");
+});
+
+app.listen(3000, () => console.log("Server started on port 3000"));
