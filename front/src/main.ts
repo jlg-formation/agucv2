cae02aeedd6289297cf80213f19408054592169f
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-07-20 15:22:56 +0200

    added js-sha1 for ts declaration example

--- front/src/main.ts
+++ front/src/main.ts
@@ -1,5 +1,7 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+// this is temporary, just for the show.
+import * as sha1 from 'js-sha1';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
@@ -8,5 +10,8 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.error(err));
+
+console.log('test sha1', sha1('1234'));
