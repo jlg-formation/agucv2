94bd129879c5f4c0b97cd56a32b0b18d4593d737
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-07-21 15:33:24 +0200

    added reactive form

--- front/src/app/stock/stock.module.ts
+++ front/src/app/stock/stock.module.ts
@@ -1,16 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { StockRoutingModule } from './stock-routing.module';
 import { StockComponent } from './stock.component';
 import { CreateComponent } from './create/create.component';
 
-
 @NgModule({
   declarations: [StockComponent, CreateComponent],
-  imports: [
-    CommonModule,
-    StockRoutingModule
-  ]
+  imports: [CommonModule, StockRoutingModule, ReactiveFormsModule],
 })
-export class StockModule { }
+export class StockModule {}
