9e55419ef4756405948a1e856208a375bbe09372
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-07-21 14:58:15 +0200

    add create component + table + observable

--- front/src/app/stock/stock.component.ts
+++ front/src/app/stock/stock.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { Article } from 'src/app/interfaces/article';
 
 @Component({
   selector: 'app-stock',
   templateUrl: './stock.component.html',
-  styleUrls: ['./stock.component.scss']
+  styleUrls: ['./stock.component.scss'],
 })
 export class StockComponent implements OnInit {
+  articles$ = new BehaviorSubject<Article[]>([
+    { name: 'Tournevis', price: 3.99, qty: 100 },
+    { name: 'Tournevis', price: 3.99, qty: 100 },
+    { name: 'Tournevis', price: 3.99, qty: 100 },
+    { name: 'Tournevis', price: 3.99, qty: 100 },
+  ]);
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
