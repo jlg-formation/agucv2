057f2067e72ff50a56f1a67d0d78b0e08d29aef9
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-07-21 16:42:51 +0200

    added appAutofocus on home

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -6,19 +6,12 @@ import { AppComponent } from './app.component';
 import { LayoutModule } from './layout/layout.module';
 import { HomeComponent } from './routes/home/home.component';
 import { LegalComponent } from './routes/legal/legal.component';
+import { WidgetModule } from './widget/widget.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    LegalComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    LayoutModule
-  ],
+  declarations: [AppComponent, HomeComponent, LegalComponent],
+  imports: [BrowserModule, AppRoutingModule, LayoutModule, WidgetModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
