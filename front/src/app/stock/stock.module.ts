e630dcac0f7bdb7f70a4b91786058ffa8797152e
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-07-21 16:26:21 +0200

    added module widget

--- front/src/app/stock/stock.module.ts
+++ front/src/app/stock/stock.module.ts
@@ -5,9 +5,10 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { StockRoutingModule } from './stock-routing.module';
 import { StockComponent } from './stock.component';
 import { CreateComponent } from './create/create.component';
+import { WidgetModule } from '../widget/widget.module';
 
 @NgModule({
   declarations: [StockComponent, CreateComponent],
-  imports: [CommonModule, StockRoutingModule, ReactiveFormsModule],
+  imports: [CommonModule, StockRoutingModule, ReactiveFormsModule, WidgetModule],
 })
 export class StockModule {}
