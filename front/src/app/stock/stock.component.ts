f197faaf857febb28f50c200c5eda8e7b460727b
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-07-22 10:48:43 +0200

    added selected and trash button

--- front/src/app/stock/stock.component.ts
+++ front/src/app/stock/stock.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { faRedo, faPlus } from '@fortawesome/free-solid-svg-icons';
+import { faRedo, faPlus, faTrashAlt } from '@fortawesome/free-solid-svg-icons';
 
 import { ArticleService } from 'src/app/services/article.service';
+import { Article } from '../interfaces/article';
 
 @Component({
   selector: 'app-stock',
@@ -11,8 +12,20 @@ import { ArticleService } from 'src/app/services/article.service';
 export class StockComponent implements OnInit {
   faRedo = faRedo;
   faPlus = faPlus;
+  faTrashAlt = faTrashAlt;
+
+  selectedArticles = [] as Article[];
 
   constructor(public articleService: ArticleService) {}
 
   ngOnInit(): void {}
+
+  toggle(article: Article): void {
+    if (this.selectedArticles.includes(article)) {
+      const index = this.selectedArticles.findIndex((a) => a === article);
+      this.selectedArticles.splice(index, 1);
+      return;
+    }
+    this.selectedArticles.push(article);
+  }
 }
