6c48be8a59ee059643e037020b99c8d250d4db95
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-07-23 10:44:15 +0200

    improved http coverage

--- front/src/app/services/http-article.service.spec.ts
+++ front/src/app/services/http-article.service.spec.ts
@@ -2,6 +2,7 @@ import { TestBed } from '@angular/core/testing';
 
 import { HttpArticleService } from './http-article.service';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { a1 } from '../mock/data';
 
 describe('HttpArticleService', () => {
   let service: HttpArticleService;
@@ -14,6 +15,21 @@ describe('HttpArticleService', () => {
   });
 
   it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('should refresh', () => {
+    service.refresh();
+    expect(service).toBeTruthy();
+  });
+
+  it('should add', () => {
+    service.add(a1);
+    expect(service).toBeTruthy();
+  });
+
+  it('should remove', () => {
+    service.remove([]);
     expect(service).toBeTruthy();
   });
 });
