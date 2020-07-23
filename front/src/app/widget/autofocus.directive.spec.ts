6e5ff9c0655a70d0eba766f57f6c74519a97fdcc
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-07-23 12:30:33 +0200

    autofocus functionaly tested

--- front/src/app/widget/autofocus.directive.spec.ts
+++ front/src/app/widget/autofocus.directive.spec.ts
@@ -1,8 +1,34 @@
 import { AutofocusDirective } from './autofocus.directive';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-test',
+  template: '<input type="text" appAutofocus>',
+})
+export class TestComponent {}
 
 describe('AutofocusDirective', () => {
-  it('should create an instance', () => {
-    // const directive = new AutofocusDirective();
-    // expect(directive).toBeTruthy();
+  let component: TestComponent;
+  let fixture: ComponentFixture<TestComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [TestComponent, AutofocusDirective],
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TestComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should focus on the element where appAutofocus is applied', () => {
+    const actualAutofocus = document.activeElement;
+    const compiled: HTMLElement = fixture.nativeElement;
+    const expectedAutofocus = compiled.querySelector('input');
+    expect(actualAutofocus).toBe(expectedAutofocus);
   });
 });
