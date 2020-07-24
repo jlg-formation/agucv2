ef36a617a082b5a93dcbdf055faf17de26925174
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-07-24 17:48:24 +0200

    added loading on create

--- front/src/app/stock/create/create.component.ts
+++ front/src/app/stock/create/create.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Router, ActivatedRoute } from '@angular/router';
-import { Store } from '@ngrx/store';
+import { Store, select } from '@ngrx/store';
 
 import { ArticleService } from 'src/app/services/article.service';
 import { Article } from 'src/app/interfaces/article';
 import { stockRoute } from 'src/app/misc/routes';
-import { AppState } from 'src/app/reducers';
+import { AppState, selectArticleLoading } from 'src/app/reducers';
 import { addArticle } from 'src/app/actions/article.actions';
+import { faSpinner } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-create',
@@ -24,12 +25,19 @@ export class CreateComponent implements OnInit {
     qty: new FormControl(100, Validators.required),
   });
 
+  loading = false;
+  faSpinner = faSpinner;
+
   constructor(
     private router: Router,
     private route: ActivatedRoute,
     private articleService: ArticleService,
     private store: Store<AppState>
-  ) {}
+  ) {
+    this.store
+      .pipe(select(selectArticleLoading))
+      .subscribe((loading) => (this.loading = loading));
+  }
 
   ngOnInit(): void {}
 
